d506807158a9fc09296561db9db5ad004dc82ec6
Léo K <leo.kontente@gmail.com>
2024-05-07 00:19:59 +0200

    avancement dans de day3, mais résultat incorrect, erreur inconnu a chercher

---
+++ test.js
@@ -0,0 +1,8 @@
+// Chaîne de caractères de 34 caractères
+let chaineComplete = "0123456789abcdefghijABCDEFGHIJ";
+
+// Extraction des 6 caractères à partir du 11ème caractère
+let chaineExtraite = chaineComplete.substr(0+2, 5+3);
+
+// Affichage du résultat
+console.log(chaineComplete + " " + chaineExtraite);
